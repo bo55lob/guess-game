e8634e0dd4c53bd213110e191dcdd078cfdf56a1
Charlie Kempson <kempsoncharlie@gmail.com>
2026-08-09 09:19:40 +0100

    Fixed

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_SOCKET_URL || window.location.origin);
+const socketUrl =
+  import.meta.env.VITE_SOCKET_URL ||
+  (import.meta.env.DEV ? "http://localhost:3001" : window.location.origin);
+
+const socket = io(socketUrl);
 
 export default socket;
